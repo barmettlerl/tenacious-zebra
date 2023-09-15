5fd86fbbb1f8ffcf3bf3206a6dc5f060b726d4b4
theMaimu <barmettler.lars@gmail.com>
2023-09-15 16:00:13 +0200

    add Higher-Rank Trait Bounds to Field

--- src/common/store/field.rs
+++ src/common/store/field.rs
@@ -1,5 +1,5 @@
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
-pub trait Field: 'static + Serialize + Send + Sync {}
+pub trait Field: 'static + Serialize + for<'de> Deserialize<'de> + Send + Sync {}
 
-impl<T> Field for T where T: 'static + Serialize + Send + Sync {}
+impl<T> Field for T where for<'de> T: 'static + Serialize + Deserialize<'de>  + Send + Sync {}
